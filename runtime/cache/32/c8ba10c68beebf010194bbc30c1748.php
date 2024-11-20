009157e28be3cc2f85ca653c497216a24897e86f
海f <R1036333191@gmail.com>
2024-11-20 16:50:54 +0800

    Stop tracking runtime directory

--- runtime/cache/32/c8ba10c68beebf010194bbc30c1748.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000003600
- exit();?>
-a:17:{s:7:"vod_all";i:41;s:7:"vod_min";d:1;s:7:"art_min";d:0;s:10:"type_all_1";i:14;s:10:"type_all_0";i:41;s:10:"type_all_2";i:1;s:10:"type_all_3";i:2;s:10:"type_all_4";i:24;s:9:"topic_all";i:4;s:11:"topic_today";i:0;s:9:"tpoic_min";d:2;s:9:"actor_all";i:0;s:11:"actor_today";i:0;s:9:"actor_min";d:0;s:8:"role_all";i:0;s:10:"role_today";i:0;s:8:"role_min";d:0;}
