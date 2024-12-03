d7e763957efa0ba08e269cbe10db475fea7b7ece
海f <R1036333191@gmail.com>
2024-12-03 12:14:26 +0800

    Auto upload

--- application/common/extend/pay/Shayu.php
+++ application/common/extend/pay/Shayu.php
@@ -11,7 +11,7 @@ class Shayu {
         $data['channelType'] = $GLOBALS['config']['pay']['shayu']['channel_type'];
         $data['notifyUrl'] = $GLOBALS['http_type'] . $_SERVER['HTTP_HOST'] . '/index.php/payment/notify/pay_type/shayu';
         $data['sign'] = $this->sign($data, $GLOBALS['config']['pay']['shayu']['appkey']);
-        print_r($data);exit;
+        // print_r($data);exit;
 
         $sHtml = "<form id='shayusubmit' name='shayusubmit' action='{$GLOBALS['config']['pay']['shayu']['apiurl']}' method='POST'>";
         while (list ($key, $val) = each ($data)) {
